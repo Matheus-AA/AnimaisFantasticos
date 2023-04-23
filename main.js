5919daa2a202e198f5ffb40778377cf5c598d33c
Matheus-AA <matheus2000_alves@hotmail.com>
2023-04-23 18:29:07 -0300

    Final

---
+++ main.js
@@ -0,0 +1,16 @@
+document.documentElement.className += ' js';
+const $tabMenu = document.querySelectorAll('.js-tabmenu li');
+const $tabContent = document.querySelectorAll('.js-tabcontent section');
+
+if ($tabMenu.length && $tabContent.length) {
+  const activeTab = (index) => {
+    $tabContent.forEach((section) => {
+      section.classList.remove('ativo');
+    });
+    $tabContent[index].classList.add('ativo');
+  };
+
+  $tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener('click', () => activeTab(index));
+  });
+}
